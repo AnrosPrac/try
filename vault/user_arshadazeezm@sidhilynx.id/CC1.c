364bee01baf2af70ebebb626708cb4d52e14a03d
Lumetrics Engine <engine@lumetrics.ai>
2026-01-13 20:39:24 +0000

    Sync: arshadazeezm@sidhilynx.id

--- vault/user_arshadazeezm@sidhilynx.id/CC1.c
+++ vault/user_arshadazeezm@sidhilynx.id/CC1.c
@@ -1,23 +1,45 @@
 #include <stdio.h>
 
-int main()
-{
-    int n = 0;
-    float mark,average,temp,sum = 0.0;
-    printf("Enter the number of students : \n");
-    scanf("%d",&n);
-    
-    for(int i = 1;i<=n;i++){
-        printf("Enter mark %d : ",i);
-        scanf("%f",&temp);
-        sum=sum+temp;
-        
-        
+int binarySearch(int arr[], int size, int target) {
+    int low = 0;
+    int high = size - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        }
+
+        if (arr[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
     }
-    average=sum/n;
-    printf("Average is %f",average);
-    
-  
+
+    return -1;
+}
+
+int main() {
+    int numbers[] = {10, 20, 30, 40, 50, 70, 80, 90};
+    int n = sizeof(numbers) / sizeof(numbers[0]);
+    int key1 = 70;
+    int key2 = 60;
+    int index1 = binarySearch(numbers, n, key1);
+    int index2 = binarySearch(numbers, n, key2);
+
+    if (index1 != -1) {
+        printf("Element %d found at index %d.\n", key1, index1);
+    } else {
+        printf("Element %d not found in the array.\n", key1);
+    }
+
+    if (index2 != -1) {
+        printf("Element %d found at index %d.\n", key2, index2);
+    } else {
+        printf("Element %d not found in the array.\n", key2);
+    }
 
     return 0;
 }
