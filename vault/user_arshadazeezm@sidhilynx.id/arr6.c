6124620711ec99d6b6fd65016d1b3b3dc81ef863
Lumetrics Engine <engine@lumetrics.ai>
2026-01-14 07:04:49 +0000

    Sync: arshadazeezm@sidhilynx.id

--- vault/user_arshadazeezm@sidhilynx.id/arr6.c
+++ vault/user_arshadazeezm@sidhilynx.id/arr6.c
@@ -11,30 +11,6 @@ int linearSearch(int arr[], int size, int target) {
 
 int main() {
     int numbers[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-    int n = sizeof(numbers) / sizeof(numbers[0]);
-
-    int key1 = 40;
-    int key2 = 110;
-
-    // Perform the linear search for key1
-    int index1 = linearSearch(numbers, n, key1);
-
-    // Check if key1 was found and print the result
-    if (index1 != -1) {
-        printf("Element %d found at index %d.\n", key1, index1);
-    } else {
-        printf("Element %d not found in the array.\n", key1);
-    }
-
-    // Perform the linear search for key2
-    int index2 = linearSearch(numbers, n, key2);
-
-    // Check if key2 was found and print the result
-    if (index2 != -1) {
-        printf("Element %d found at index %d.\n", key2, index2);
-    } else {
-        printf("Element %d not found in the array.\n", key2);
-    }
-
+    int n 
     return 0;
 }
