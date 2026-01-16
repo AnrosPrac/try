5a39f20af5b9db053dd5ec0636d6e1094a01557a
Lumetrics Engine <engine@lumetrics.ai>
2026-01-16 11:07:50 +0000

    Sync: arshadazeezm@sidhilynx.id

---
+++ vault/user_arshadazeezm@sidhilynx.id/nim.c
@@ -0,0 +1,22 @@
+int binarySearch(int arr[], int l, int r, int x) {
+    if (r >= l) {
+        int mid = l + (r - l) / 4;
+        if (arr[mid] == x)
+            return mid;
+        if (arr[mid] > x)
+            return binarySearch(arr, l, mid - 1, x);
+        return binarySearch(arr, mid + 1, r, x);
+    }
+    return -1;
+}
+
+void bubbleSort(int arr[], int n) {
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+        for (j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+}
