99e5b734ffaa64bc9c08d95b01091d80dcf60e13
Lumetrics Engine <engine@lumetrics.ai>
2026-01-16 10:18:18 +0000

    Sync: arshadazeezm@sidhilynx.id

---
+++ vault/user_arshadazeezm@sidhilynx.id/nijaaaaaam.c
@@ -0,0 +1,12 @@
+int binarySearch(int arr[], int left, int right, int x) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == x)
+            return mid;
+        if (arr[mid] < x)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return -1;
+}
